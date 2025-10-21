868ef3e25315cc0119c771c9c7bc75761c119e98
KernelNest <121437369+devtunis@users.noreply.github.com>
2025-10-21 20:47:36 +0100

    Update longestConsecutive.java
    add solution in python

--- longestConsecutive.java
+++ longestConsecutive.java
@@ -105,3 +105,24 @@ return longestSequence;
 }
 
 
+
+nums = [10,1,3,7,13,11,2,4]
+
+def consecutiveNumber(nums):
+    num_set = set(nums)
+    longest = 0
+
+    for num in num_set:
+        if num - 1 not in num_set:  
+            current = num
+            length = 1
+            while current + 1 in num_set:
+                current += 1
+                length += 1
+            longest = max(longest, length)
+    return longest
+
+print(consecutiveNumber(nums))  # 4
+
+
+// i love this solution also in python
